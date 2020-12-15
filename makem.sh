0bc63a704d4d54d9635a13e1c8b7a810054709ba
Kevin Nash <knash201@gmail.com>
2020-12-15 16:07:47 -0500

    facapps v0

---
+++ makem.sh
@@ -0,0 +1,10 @@
+rm RI2020.log RI2020.aux RI2020.out RI2020.blg RI2020.bbl
+pdflatex RI2020
+bibtex  RI2020
+pdflatex RI2020
+pdflatex RI2020
+
+pdflatex TS2020
+pdflatex DS2020
+pdflatex CV2020
+pdflatex PL2020
